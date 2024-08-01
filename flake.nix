30986f0858a89c69f0e5e14da2b532c7da987010
Tom Bereknyei <tomberek@gmail.com>
2024-08-01 02:51:53 -0400

    feat: add simple defaults and lock

--- flake.nix
+++ flake.nix
@@ -12,5 +12,10 @@
         in
         result;
       usingFor = pkgGroup: recipes: builtins.mapAttrs (_: pkgs: using pkgs recipes) pkgGroup;
+      usingPkgs = pkgGroup: recipes: usingFor pkgGroup (recipes // {
+        default = {buildEnv,pkgs}: buildEnv {
+          name = "default-env";
+          paths = builtins.attrValues (using pkgs recipes);
+      };});
     };
 }
