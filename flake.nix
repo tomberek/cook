a7ded49bc28a69548207c2d6c3a580491dac711c
Tom Bereknyei <tomberek@gmail.com>
2024-08-01 02:59:16 -0400

    feat: add overlays

--- flake.nix
+++ flake.nix
@@ -1,29 +1,45 @@
 {
   outputs =
-    { self, ... }: rec {
+    { self, ... }:
+    rec {
       using =
         pkgs: recipes:
         let
           result = builtins.mapAttrs (
             name: recipe:
-            if recipe == {} then pkgs.${name} else
-            pkgs.lib.callPackageWith (pkgs // result) recipe { }
+            if recipe == { } then pkgs.${name} else pkgs.lib.callPackageWith (pkgs // result) recipe { }
           ) recipes;
         in
         result;
       usingFor = pkgGroup: recipes: builtins.mapAttrs (_: pkgs: using pkgs recipes) pkgGroup;
-      usingPkgs = pkgGroup: recipes: usingFor pkgGroup (recipes // {
-        default = {buildEnv,pkgs}: buildEnv {
-          name = "default-env";
-          paths = builtins.attrValues (using pkgs recipes);
-      };});
-      usingShells = pkgGroup: recipes: usingFor pkgGroup (recipes // {
-        default =
-          { mkShell, pkgs }:
-          mkShell {
-            name = "default-shell";
-            packages = builtins.attrValues (using pkgs recipes);
-          };
-      });
+      usingPkgs =
+        pkgGroup: recipes:
+        usingFor pkgGroup (
+          recipes
+          // {
+            default =
+              { buildEnv, pkgs }:
+              buildEnv {
+                name = "default-env";
+                paths = builtins.attrValues (using pkgs recipes);
+              };
+          }
+        );
+      usingShells =
+        pkgGroup: recipes:
+        usingFor pkgGroup (
+          recipes
+          // {
+            default =
+              { mkShell, pkgs }:
+              mkShell {
+                name = "default-shell";
+                packages = builtins.attrValues (using pkgs recipes);
+              };
+          }
+        );
+      usingOverlays =
+        recipes: self: super:
+        builtins.mapAttrs (name: recipe: self.callPackage recipe { }) recipes;
     };
 }
